3cc62b5fc8cae337dfa41850aaaa34e82922fe43
frank <frank@ubuntu.(none)>
2014-08-13 19:27:49 +0800

    Minor changes

--- zeek2.js
+++ zeek2.js
@@ -312,11 +312,13 @@ levelData = [
 		stage.setObject(16, 11, new Wall(3));
 
 		/* Custom script */
+		/*
 		var image = new Image();
 		image.src = 'egg/test.jpg';
 		stage.getObject(16, 2).onDraw = function(ctx, col, row) {
 			ctx.drawImage(image, IMAGE_W * 14, 0, IMAGE_W * 3, IMAGE_H * 3);
 		}
+		*/
 	}),
 	/* Level 3 */
 	new Stage(function(stage){
