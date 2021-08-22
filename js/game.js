4dfde4a5d072ec26cc6f0d006987969bbf01bcfa
frank-deng <13382084679@163.com>
2021-08-22 13:49:05 +0800

    Use localStorage to store puzzle pack score and current level

--- js/game.js
+++ js/game.js
@@ -5,11 +5,24 @@ function puzzlePackFinished(totalScore) {
 	$('div#gamefinished').show();
 	$('div#gamefinished h1#totalscore').html(totalScore);
 	//Replay puzzle pack
+	try{
+		let storageData=localStorage.getItem('puzzleProcess');
+		if(storageData){
+			storageData=JSON.parse(storageData);
+			delete storageData[packName + '_Level'];
+			delete storageData[packName + '_Score'];
+			localStorage.setItem('puzzleProcess',JSON.stringify(storageData));
+		}
+	}catch(e){
+		console.error(e);
+	}
+	/*
 	$('input#replay').bind('click', function(event){
 		$.cookie(packName + '_Level', null, {expires: -1});
 		$.cookie(packName + '_Score', null, {expires: -1});
 		location = location;	//Refresh page
 	});
+	*/
 }
 function Game(packName, levelData) {
 	var ctx = $('canvas#stage')[0].getContext('2d');
@@ -20,11 +33,21 @@ function Game(packName, levelData) {
 	$('input#enableSound')[0].checked = false;
 
 	//Get puzzle pack stored data
-	totalScore = $.cookie(packName + '_Score');
+	try{
+		let storageData=localStorage.getItem('puzzleProcess');
+		if(storageData){
+			storageData=JSON.parse(storageData);
+			totalScore=storageData[packName + '_Score'] || undefined;
+			levelNum=storageData[packName + '_Level'] || undefined;
+		}
+	}catch(e){
+		console.error(e);
+	}
+	//totalScore = $.cookie(packName + '_Score');
 	if (undefined != totalScore && !isNaN(totalScore)) {
 		totalScore = Number(totalScore);
 	}
-	levelNum = $.cookie(packName + '_Level');
+	//levelNum = $.cookie(packName + '_Level');
 	if (undefined != levelNum && !isNaN(levelNum)) {
 		levelNum = Number(levelNum);
 	} else {
@@ -44,8 +67,23 @@ function Game(packName, levelData) {
 	$(document).bind('levelFinished', function(event, levelScore){
 		totalScore += levelScore;
 		levelNum++;
+		try{
+			let storageData=localStorage.getItem('puzzleProcess');
+			if(storageData){
+				storageData=JSON.parse(storageData);
+			}else{
+				storageData={};
+			}
+			storageData[packName + '_Level']=levelNum;
+			storageData[packName + '_Score']=totalScore;
+			localStorage.setItem('puzzleProcess',JSON.stringify(storageData));
+		}catch(e){
+			console.error(e);
+		}
+		/*
 		$.cookie(packName + '_Level', levelNum, {expires: 3650});
 		$.cookie(packName + '_Score', totalScore, {expires: 3650});
+		*/
 		if (levelNum < levelData.length) {
 			//Next level
 			level = new Level(levelData[levelNum], ctx);
